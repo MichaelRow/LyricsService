0f59c9bf29b078d3e61b59348740758afe8d684d
Michael Row <little93@me.com>
2017-09-16 22:03:46 +0800

    Initial Commit

---
+++ LyricsService/LyricsService.h
@@ -0,0 +1,19 @@
+//
+//  LyricsService.h
+//  LyricsService
+//
+//  Created by Michael Row on 2017/9/16.
+//  Copyright © 2017年 Michael Row. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LyricsService.
+FOUNDATION_EXPORT double LyricsServiceVersionNumber;
+
+//! Project version string for LyricsService.
+FOUNDATION_EXPORT const unsigned char LyricsServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LyricsService/PublicHeader.h>
+
+
